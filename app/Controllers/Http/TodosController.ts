02066081dd000d9b58434da11e2306f3cf6030f1
tmicka23 <mickathp42@gmail.com>
2022-04-25 14:35:58 +0200

    feat: make crud

--- app/Controllers/Http/TodosController.ts
+++ app/Controllers/Http/TodosController.ts
@@ -1,13 +1,31 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Todo from 'App/Models/Todo'
 
 export default class TodosController {
-  public async index({}: HttpContextContract) {}
+  public async index({response}: HttpContextContract) {
+    const todos = await Todo.all()
+    response.json(todos)
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({request, response}: HttpContextContract) {
+    const todo = await Todo.create(request.body(), { allowExtraProperties: true })
+    response.status(201).json(todo)
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({response, params}: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id)
+    response.json(todo)
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({request, response, params}: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id)
+    todo.merge(request.body(), true)
+    await todo.save()
+    response.json(todo)
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({params, response}: HttpContextContract) {
+    await (await Todo.findOrFail(params.id)).delete()
+    response.status(204)
+  }
 }
